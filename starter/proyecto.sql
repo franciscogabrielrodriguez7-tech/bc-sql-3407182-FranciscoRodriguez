03e1659d6f31dd595e2e868a7bad6c35f875d241
gabielrodriguez <franciscogabrielrodriguez7-tech>
2026-06-26 21:04:23 -0500

    feat(sql): add basic queries for flights, aircraft, and passengers

--- starter/proyecto.sql
+++ starter/proyecto.sql
@@ -1,9 +1,20 @@
 .tables
 
-SELECT *
+SELECT 
+    origin AS origen,
+    destination AS destino,
+    flight_number AS num_vuelo
 FROM flights;
 
-SELECT name
+SELECT 
+    model AS modelo,
+    capacity AS capacidad
+FROM aircraft
+WHERE capacity > 100;
+
+SELECT 
+    name AS nombre,
+    lastname AS apellido
 FROM passengers
 ORDER BY name asc;
 
@@ -12,4 +23,3 @@ FROM flights;
 
 SELECT COUNT(*) AS total_passengers
 FROM passengers;
-
